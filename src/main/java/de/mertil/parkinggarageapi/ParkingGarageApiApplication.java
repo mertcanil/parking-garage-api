99e8ed3a899b55143142546054513551b074b6b9
mert.il <mert.il@cyberinnovations.de>
2022-05-27 21:48:35 +0200

    init spring

---
+++ src/main/java/de/mertil/parkinggarageapi/ParkingGarageApiApplication.java
@@ -0,0 +1,13 @@
+package de.mertil.parkinggarageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingGarageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingGarageApiApplication.class, args);
+	}
+
+}
